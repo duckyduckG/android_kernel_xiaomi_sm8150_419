cb30453a8a502c9c338eac04cb21cfb91edae217
UtsavBalar1231 <utsavbalar1231@gmail.com>
2025-10-20 23:22:39 +0500

    techpack: camera-msmnile: Import Xiaomi changes
    [duckyduck: add device specific configs]
    
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Change-Id: I20a82c9e77f92405d77a0d16621ce12209dfd194

---
+++ techpack/camera-msmnile/config/crux.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#define CONFIG_VCM_AK7374 1
+#define CONFIG_USE_ROHM_BU64753 1
