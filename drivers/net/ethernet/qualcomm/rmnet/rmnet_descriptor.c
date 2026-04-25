8ec54b41695ed9fb3c561e85fc66f0ccdccb9ce1
duckyduckG <duckyduckg65@gmail.com>
2026-04-25 11:16:38 +0500

    Revert "core: Remove dl marker v1 usage"
    reason: breaks mobile data on sm8150.
    
    This reverts commit 7f90265b8e0a10fc7c68ac38bb5b9691f08cc23c.
    
    Change-Id: I8081bfe56619c72728887864dc9d554a826e5731

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_descriptor.c
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_descriptor.c
@@ -236,6 +236,8 @@ rmnet_frag_process_flow_start(struct rmnet_map_control_command_header *cmd,
 	 */
 	if (is_dl_mark_v2)
 		rmnet_map_dl_hdr_notify_v2(port, dlhdr, cmd);
+	else
+		rmnet_map_dl_hdr_notify(port, dlhdr);
 }
 
 static void
@@ -262,6 +264,8 @@ rmnet_frag_process_flow_end(struct rmnet_map_control_command_header *cmd,
 	 */
 	if (is_dl_mark_v2)
 		rmnet_map_dl_trl_notify_v2(port, dltrl, cmd);
+	else
+		rmnet_map_dl_trl_notify(port, dltrl);
 }
 
 /* Process MAP command frame and send N/ACK message as appropriate. Message cmd
